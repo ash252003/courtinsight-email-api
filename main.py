9607dc8744dab456cc6dcf92c3ed9855c86c4b56
Ashish Yadav <154996701+ash252003@users.noreply.github.com>
2026-08-14 22:59:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,26 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import smtplib
-from email.mime.text import MIMEText
-import os
-
-app = FastAPI()
-
-SMTP_EMAIL = os.getenv("SMTP_EMAIL")
-SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
-
-
-class EmailRequest(BaseModel):
-    to: str
-    subject: str
-    message: str
-
-
 @app.post("/send-email")
 def send_email(req: EmailRequest):
-    try:
-        msg = MIMEText(req.message)
-        msg["Subject"] = req.subject
-        msg["From"] = SMTP_EMAIL
-        msg["To"] = req.to
+    print("===== REQUEST RECEIVED =====")
+    print(req)
 
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(SMTP_EMAIL, SMTP_PASSWORD)
-            server.send_message(msg)
+    msg = MIMEText(req.message)
+    msg["Subject"] = req.subject
+    msg["From"] = SMTP_EMAIL
+    msg["To"] = req.to
 
-        return {
-            "success": True,
-            "message": "Email sent successfully"
-        }
+    print("Connecting to Gmail...")
 
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=str(e)
-        )
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, timeout=15) as server:
+        print("Connected")
+
+        print("Logging in...")
+        server.login(SMTP_EMAIL, SMTP_PASSWORD)
+
+        print("Logged in")
+
+        print("Sending...")
+        server.send_message(msg)
+
+        print("Sent")
+
+    return {"status": "Email sent"}
